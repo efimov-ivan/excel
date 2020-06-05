da5c4a00a7856b0021625ecf6dbf536dddde5c1c
Vlad <i.efimov.jamteam@gmail.com>
2020-06-06 02:25:02 +0600

    finish table resize

---
+++ src/components/table/table.resize.js
@@ -0,0 +1,38 @@
+import {$} from '@core/dom'
+
+export function resizeHandler($root, event) {
+  const $resizer = $(event.target)
+  const $parent = $resizer.closest('[data-type="resizable"]')
+  const coords = $parent.getCoords()
+  const type = $resizer.data.resize
+  let value
+
+  document.onmousemove = e => {
+    if (type === 'col') {
+      const delta = Math.floor(e.pageX - coords.right)
+      value = coords.width + delta
+      $resizer.css({right: -delta + 'px', bottom: '-5000px', opacity: 1})
+    } else {
+      const delta = e.pageY - coords.bottom
+      value = coords.height + delta
+      $resizer.css({opacity: 1, right: '-5000px', bottom: -delta + 'px'})
+    }
+  }
+
+  document.onmouseup = () => {
+    document.onmouseup = null
+    document.onmousemove = null
+
+    if (type === 'col') {
+      $root.findAll(`[data-col="${$parent.data.col}"]`)
+          .forEach(e => e.style.width = value + 'px')
+    } else {
+      $parent.css({height: value + 'px'})
+    }
+    $resizer.css({
+      opacity: 0,
+      bottom: 0,
+      right: 0
+    })
+  }
+}
